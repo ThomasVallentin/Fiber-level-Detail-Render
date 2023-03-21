210f6a6a269dd7d4794bd350715983816e46aa69
Thomas Vallentin <vallentin.thomas01@gmail.com>
2023-03-21 21:42:49 +0100

    Forgot shaders in the previous commit, Shadow maps are rendered using a custom shader as we don't render all the fibers, but only the yarns as tubes. The ones from the commit are temporary as they will need to match the yarn/ply generation

---
+++ src/shaders/noProjection3D.vs.glsl
@@ -0,0 +1,26 @@
+#version 460 core
+
+layout(location = 0) in vec3 aPosition;
+layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aTexCoord;
+
+uniform mat4 uModelMatrix = mat4(1.0);
+uniform mat4 uViewMatrix = mat4(1.0);
+
+out VertexData
+{
+    vec3 position;
+    vec3 normal;
+    vec2 texCoord;
+} outVertex;
+
+
+void main() 
+{
+    vec4 position = uViewMatrix * uModelMatrix * vec4(aPosition, 1.0);
+    outVertex.position = position.xyz;
+    // outVertex.normal = (uViewMatrix * uModelMatrix * vec4(aNormal, 1.0)).xyz;
+    outVertex.texCoord = aTexCoord;
+
+    gl_Position = position;
+}
